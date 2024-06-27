e23d7309d0ec15af36de6c786fdaa5b3365f4ce5
PranavPradeep390 <pranavpradeep786@gmail.com>
2024-06-27 11:19:07 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Institution from './pages/Institution';
+import NewsandEvents from './pages/NewsandEvents';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header ">
-        
-        <p className=' bg-red-500'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/institution' element={<Institution/>}/>
+      <Route path='/newsandevents' element={<NewsandEvents/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+
+    </Routes>
+
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,62 @@
+
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { Link, useLocation } from 'react-router-dom';
+import BtagLogo from '../../assets/BtagLogo.png'
+
+function Header() {
+  const location = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <div className='min-h-[80px] grid sm:grid-cols-12 sm:w-auto w-full shadow-lg'>
+        {/* titleSection */}
+        <div className='flex sm:justify-start items-center sm:p-4 justify-between p-4 sm:col-span-5'>
+          <a href="/" className="elementor-widget-cmsmasters-site-logo__link" rel="nofollow">
+            <img  src={BtagLogo}  alt=""
+              title="BtagLogo"
+              className="cmsmasters-lazyload-disabled elementor-widget-cmsmasters-site-logo__img elementor-widget-cmsmasters-site-logo-main w-18 h-14"/>
+          </a>
+          <div className='ms-0 md:ms-3'>
+            <p className='md:text-xl text-base font_poppins font-bold text-[#585858]'>BTAG Global Educational Trust</p>
+          </div>
+          <div className='sm:hidden'>
+            <button onClick={toggleMenu}>
+              {isOpen ? <FaTimes className='text-2xl' /> : <FaBars className='text-2xl' />}
+            </button>
+          </div>
+        </div>
+
+        {/* navlinkssection */}
+        <div className={`mb-5 md:mb-0 flex-col gap-5 space-y-4 sm:space-y-0 sm:flex-row sm:space-x-10 md:justify-center justify-start items-center sm:flex ${isOpen ? 'flex' : 'hidden'} sm:block sm:p-4 sm:col-span-7`}>
+          <Link to="/" onClick={closeMenu}>
+            <h3 className={`text-lg uppercase font-light font_abeezee hover:text-[#73AB3A] ${location.pathname==="/"?"text-[#73AB3A] underline":" "}`}>Home</h3>
+          </Link>
+          <Link to='/about' onClick={closeMenu}>
+            <h3 className={`text-lg uppercase font-light font_abeezee hover:text-[#73AB3A]  ${location.pathname==='/about'?"text-[#73AB3A] underline":" "}`}>About</h3>
+          </Link>
+          <Link to='/institution' onClick={closeMenu}>
+            <h3 className={`text-lg uppercase font-light font_abeezee hover:text-[#73AB3A] ${location.pathname==='/institution'?"text-[#73AB3A] underline":" "}`}>institutions</h3>
+          </Link>
+          <Link to='/newsandevents' onClick={closeMenu}>
+            <h3 className={`text-lg uppercase font-light font_abeezee hover:text-[#73AB3A] ${location.pathname==='/newsandevents'?"text-[#73AB3A] underline":" "}`}>news & events</h3>
+          </Link>
+          <Link to='/contact' onClick={closeMenu}>
+            <h3 className={`text-lg uppercase font-light font_abeezee hover:text-[#73AB3A] ${location.pathname==='/contact'?"text-[#73AB3A] underline":" "}`}>Contact</h3>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
