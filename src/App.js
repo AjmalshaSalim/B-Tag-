7da3be009ba8b76c08ae578ed73d58e7571735b1
PranavPradeep390 <pranavpradeep786@gmail.com>
2024-06-27 11:49:24 +0530

    footer started

--- src/App.js
+++ src/App.js
@@ -26,25 +26,8 @@ function App() {
     </>
   )
 
-    <div className="App">
-      <header className="App-header ">
-        
-        <p className=' bg-red-500'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>shreya</h1>
-        <h1>Janna</h1>
-      </header>
-    </div>
-  );
+   
+
 
 }
 
