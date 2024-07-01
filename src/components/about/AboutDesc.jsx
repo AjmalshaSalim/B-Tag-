dd9e44c7f2cdcded9360ef98b1c9ecf38d2a3325
janna <jannakondeth5@gmail.com>
2024-07-01 11:18:02 +0530

    add institution page

--- src/components/about/AboutDesc.jsx
+++ src/components/about/AboutDesc.jsx
@@ -3,21 +3,28 @@ import '../../index.css'
 
 function AboutDesc() {
   return (
-    <div className='flex justify-center items-center min-h-[700px] mx-auto container overflow-hidden px-[38px]'>
-        <div className=' w-full min-h-[600px] flex justify-center lg:items-start items-center flex-col space-y-2 overflow-hidden mx-[40px] lg:pt-0 pt-[30px]'> 
+    <>
+    
+    <div className='flex justify-center items-center min-h-[600px]  overflow-hidden sm:px-[38px] px-[8px]  '>
+        
+        <div className=' w-full min-h-[480px] flex justify-start lg:items-start items-center flex-col space-y-2 overflow-hidden mx-[40px]  '> 
 
-        <div className='p-4 ' >
-            <h1 className='text-[#585858] font_poppins font-semibold 2xl:text-[38px] lg:text-[35px] text-[12px] sm:text-[30px] lg:ml-[60px] '>
-                <span className="underline-with-space ">About</span> BTAG Global Educational Trust
-            </h1>
-        </div>
-        <div className='grid lg:grid-cols-2 '>
-            <div className='p-4'>
+        <div className='p-4 md:mt-[20px] lg:mt-0' >
+    <h1 className='text-[#585858] font_poppins font-semibold 2xl:text-[38px]  md:text-3xl sm:text-2xl text-sm   lg:ml-[60px]  sm:text-left text-center'>
+        <span className="underline-with-space ">About</span> BTAG Global Educational Trust
+    </h1>
+</div>
+
+
+        <div className='grid lg:grid-cols-2  '>
+            <div className='p-4 '>
                 <p className='lg:ml-[60px] font_poppins font-[400px] 2xl:text-[18px] sm:text-[16px] text-[12px] leading-[33px] xl:leading-[35px] 2xl:leading-[40px] '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, cum explicabo. In, dolore! Cum commodi ipsam enim. Voluptas fuga repudiandae porro reprehenderit suscipit, perferendis perspiciatis voluptatum qui ut sequi ab!Loremorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, cum explicabo. In, dolore! Cum commodi ipsam en uptatum qui ut sequi ab!Loremorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, cum explicabo. In, doloi ut sequi ab!Loremorem ipsum d  !</p>
             </div>
-            <div className=' '>
-                <div className=' bg-red-600 h-full w-full rounded-3xl overflow-hidden'>
-                <img src='https://images.pexels.com/photos/269077/pexels-photo-269077.jpeg?auto=compress&cs=tinysrgb&w=600' alt='aboutImage1' className='w-full h-full' />
+
+
+            <div className='  p-4 lg:mr-[60px]'>
+                <div className='  h-full w-full rounded-3xl overflow-hidden'>
+                <img src='https://images.pexels.com/photos/5989927/pexels-photo-5989927.jpeg?auto=compress&cs=tinysrgb&w=600' alt='aboutImage1' className='w-full h-full' />
                 </div>
                 
             </div>
@@ -27,6 +34,7 @@ function AboutDesc() {
            
         </div>
     </div>
+    </>
   )
 }
 
---
+++ src/components/institution/InstitutionCards.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
+const cardData = [
+    {
+      id:1,
+      imageUrl: 'https://images.pexels.com/photos/6457579/pexels-photo-6457579.jpeg?auto=compress&cs=tinysrgb&w=600',
+      title: 'Cert IV in Leadership & Management ',
+      description: 'Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country... ',
+    },
+    {
+      id:2,
+      imageUrl: 'https://images.pexels.com/photos/6457544/pexels-photo-6457544.jpeg?auto=compress&cs=tinysrgb&w=600',
+      title: 'Cert IV in communication & Digital Marketing ',
+      description: 'Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country ...',
+    },
+    {
+      id:3,
+      imageUrl: 'https://images.pexels.com/photos/3760263/pexels-photo-3760263.jpeg?auto=compress&cs=tinysrgb&w=600',
+      title: 'Cert IV Hospitality ',
+      description: 'Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country ...',
+    },
+    
+  ];
+
+function InstitutionCards() {
+
+  const navigate=useNavigate()
+
+  const handleCardClick = (id) => {
+    navigate(`/card/${id}`);
+  };
+
+  return (
+    <div className='container mx-auto flex justify-center items-center py-4 mb-[40px] '>
+
+      {/* card */}
+    <div className='flex flex-wrap justify-between gap-[70px]'>
+      {cardData.map((card, index) => (
+        <div key={index} className='flex-grow'   onClick={() => handleCardClick(card.id)}>
+          <div className='p-4 bg-white  w-full sm:w-[300px]'>
+      <div className='flex flex-col items-center'>
+        <div className='w-full h-[200px] overflow-hidden'>
+          <img
+            src={card.imageUrl}
+            alt={card.title}
+            className='w-full h-full object-cover'
+          />
+        </div>
+        <div className='p-4 '>
+        <h1 className='lg:text-lg 2xl:text-xl sm:text-md  text-sm font-semibold mt-4 text-[#67A800] font_poppins  
+          leading-6 min-h-[50px] '>{card.title}</h1>
+        <p className='text-base text-[#706E6E] mt-2 font_poppins font-normal  leading-5'>{card.description}</p>
+        </div>
+        
+      </div>
+    </div>
+        </div>
+      ))}
+    </div>
+
+
+  </div>
+  )
+}
+
+export default InstitutionCards
